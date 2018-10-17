cec172d3ebcf0ec59ceb0bc070f08d1c21150ade
hongmaoxiao <769501294@qq.com>
2018-10-17 21:44:55 +0800

    Breaking: Remove the change event object being passed to the callback & make  consistent

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 var gaze = require('gaze');
 var EventEmitter = require('events').EventEmitter;
 
+function onWatch(out, cb) {
+  return function(err, rwatcher) {
+    if (err) {
+      out.emit('error', err);
+    }
+    rwatcher.on('all', function (evt, path, old) {
+      var outEvt = {type: evt, path: path};
+      if (old) {
+        outEvt.old = old;
+      }
+      out.emit('change', outEvt);
+      if (cb) {
+        cb();
+      }
+    });
+  }
+}
+
 module.exports = function (glob, opts, cb) {
   var out = new EventEmitter();
 
@@ -9,21 +27,8 @@ module.exports = function (glob, opts, cb) {
     opts = {};
   }
 
-  var watcher = gaze(glob, opts, function (err, rwatcher) {
-    if (err) {
-      out.emit('error', err);
-    }
-    rwatcher.on('all', function(evt, path, old){
-      var outEvt = {type: evt, path: path};
-      if (old) {
-        outEvt.old = old;
-      }
-      out.emit('change', outEvt);
-      if (cb) {
-        cb(outEvt);
-      }
-    });
-  });
+
+  var watcher = gaze(glob, opts, onWatch(out, cb));
 
   watcher.on('end', out.emit.bind(out, 'end'));
   watcher.on('error', out.emit.bind(out, 'error'));
@@ -34,12 +39,12 @@ module.exports = function (glob, opts, cb) {
     return watcher.close();
   };
 
-  out.add = function () {
-    return watcher.add.apply(watcher, arguments);
+  out.add = function (glob, cb) {
+    return watcher.add(glob, onWatch(out, cb));
   };
 
-  out.remove = function () {
-    return watcher.remove.apply(watcher, arguments);
+  out.remove = function (glob) {
+    return watcher.remove(glob);
   };
 
   out._watcher = watcher;
